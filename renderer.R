b5d55039200b0fb59fe5384ae006e2cceeb0c90b
Indrajeet Patil <patilindrajeet.science@gmail.com>
2025-09-28 10:33:22 +0200

    Use docs branch for slides

--- renderer.R
+++
@@ -1,5 +0,0 @@
-# if you want to build the slides locally
-quarto::quarto_render("slides.qmd")
-
-# duplicate `slides.html` as `index.html`
-fs::file_copy("slides.html", "index.html", overwrite = TRUE)
